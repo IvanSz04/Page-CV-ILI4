616dfd4763c6077065d8770595bddd7bee43eb22
IvanSz04 <151887732+IvanSz04@users.noreply.github.com>
2023-11-24 00:41:59 -0500

    Update jv.js

--- jv.js
+++ jv.js
@@ -2,12 +2,19 @@
       // Obtener los valores de los campos
       var nombre = document.getElementById("nombre").value;
       var apellido = document.getElementById("apellido").value;
-      var genero = document.getElementById("apellido").value;
+      var opcionSeleccionada = document.querySelector('input[name="genero"]:checked');
+      var opcion = opcionSeleccionada ? opcionSeleccionada.value : "Ninguna opción seleccionada";
+      var opcionSeleccionada = document.querySelector('input[name="estudio"]:checked');
+      var estudioO = opcionSeleccionada ? opcionSeleccionada.value : "Ninguna opción seleccionada";
+      var ttlo = document.getElementById("titulo").value;
+      
 
       // Crear una cadena con los datos
-      var Datos = "Hola, mi nombre es" + nombre + "" + apellido + genero+"";
+      
+      var Datos = "Hola, mi nombre es " + nombre + " " + apellido +" y soy "+ opcion+". Hice un "+estudioO+"en"+ttlo;
 
       // Abrir una nueva ventana con los datos
       var nuevaVentana = window.open('', '_blank');
       nuevaVentana.document.write('<html><head><title>Tu Hoja de Vida</title></head><body>' + Datos + '</body></html>');
-    }
+  
+}
